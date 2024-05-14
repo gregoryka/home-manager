caf922167939b0b1c55024e531289b6aad26c9f7
Gregory Kanter <gregorykanter1@gmail.com>
2024-05-14 22:45:25 +0300

    Fix flake.nix and lock

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
   inputs = {
     # Principle inputs (updated by `nix run .#update`)
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -23,10 +24,30 @@
       url = "file+file:///dev/null";
       flake = false;
     };
-    devenv.url = "github:cachix/devenv";
+    devenv = {
+      url = "github:cachix/devenv";
+      inputs.cachix.inputs.devenv.follows = "devenv";
+    };
     nix-vscode-extensions = {
       url = "github:nix-community/nix-vscode-extensions";
-      inputs.nixpkgs.follows = "nixpkgs";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+    nixgl = {
+      url = "github:nix-community/nixGL";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
+    nix2container = {
+      url = "github:nlewo/nix2container";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
     };
   };
 
@@ -36,51 +57,10 @@
       imports = [
         inputs.nixos-flake.flakeModule
         inputs.devenv.flakeModule
+        ./devenv.nix
         ./nix/toplevel.nix
       ];
 
       flake.nix-dev-home.username = "gregorykanter";
-
-      perSystem = { config, pkgs, system, ... }: {
-        formatter = with pkgs; nixfmt;
-
-        devenv.shells.default = {
-          devenv.root = let
-            devenvRootFileContent =
-              builtins.readFile inputs.devenv-root.outPath;
-          in pkgs.lib.mkIf (devenvRootFileContent != "") devenvRootFileContent;
-
-          name = "my-project";
-
-          languages.nix = {
-            enable = true;
-            lsp.package = with pkgs; nixd;
-          };
-
-          pre-commit.hooks = {
-            deadnix.enable = true;
-            flake-checker.enable = true;
-            statix.enable = true;
-          };
-
-          imports = [
-            # This is just like the imports in devenv.nix.
-            # See https://devenv.sh/guides/using-with-flake-parts/#import-a-devenv-module
-            # ./devenv-foo.nix
-          ];
-
-          # https://devenv.sh/reference/options/
-          packages = let
-            extensions = inputs.nix-vscode-extensions.extensions.${system};
-            vscode-ext = pkgs.vscode-with-extensions.override {
-              vscode = pkgs.vscodium;
-              vscodeExtensions = [
-                # extensions.vscode-marketplace.golang.go
-                extensions.open-vsx.jnoortheen.nix-ide
-              ];
-            };
-          in [ config.packages.default vscode-ext ];
-        };
-      };
     };
 }
